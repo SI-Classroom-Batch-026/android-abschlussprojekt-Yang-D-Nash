019c2c00054aa45321ba42cc57068c77366d4bb6
Yang-D-Nash <nash.lioncorna@gmail.com>
2025-11-28 10:39:05 +0100

    Logooo

--- app/src/main/java/com/example/yangdnashabschlussprojekt/ui/component/welcome/WelcomeImage.kt
+++ app/src/main/java/com/example/yangdnashabschlussprojekt/ui/component/welcome/WelcomeImage.kt
@@ -3,11 +3,9 @@ package com.example.yangdnashabschlussprojekt.ui.component.welcome
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.yangdnashabschlussprojekt.R
@@ -15,16 +13,15 @@ import com.example.yangdnashabschlussprojekt.R
 @Composable
 fun WelcomeImage(
     modifier: Modifier = Modifier,
-    imageRes: Int = R.drawable.smartvision
+    imageRes: Int = R.drawable.smartvisionmain
 ) {
     Image(
         painter = painterResource(id = imageRes),
         contentDescription = "Willkommens Icon",
+        contentScale = ContentScale.Crop,
         modifier = modifier
-            .clip(CircleShape)
             .fillMaxWidth()
-            .height(160.dp)
-            .padding(16.dp)
+            .height(200.dp)
     )
 }
 
