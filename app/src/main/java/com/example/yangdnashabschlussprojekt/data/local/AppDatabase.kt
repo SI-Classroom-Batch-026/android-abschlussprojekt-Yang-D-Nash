65a37f4face4ac5c43e85bf0a9580e96f31fce28
Yang-D-Nash <nash.lioncorna@gmail.com>
2025-12-11 10:35:38 +0100

    okeee

---
+++ app/src/main/java/com/example/yangdnashabschlussprojekt/data/local/AppDatabase.kt
@@ -0,0 +1,13 @@
+package com.example.yangdnashabschlussprojekt.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.yangdnashabschlussprojekt.data.local.database.dao.TextHistoryDao
+import com.example.yangdnashabschlussprojekt.data.local.database.model.TextHistoryEntity
+
+@Database(entities = [TextHistoryEntity::class /* Ihre anderen Entities */], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    
+    abstract fun textHistoryDao(): TextHistoryDao
+
+}
