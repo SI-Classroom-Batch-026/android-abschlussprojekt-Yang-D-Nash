2385eb8a71e2fdd57877a22a5c7fd30c7e32942b
Yang-D-Nash <nash.lioncorna@gmail.com>
2025-12-18 20:08:48 +0100

    iOs App wiederhergestellt und KMP läuft

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,18 +1,17 @@
-//
-//  ContentView.swift
-//  iosApp
-//
-//  Created by Yang D. Nash on 18.12.25.
-//
-
-
 import SwiftUI
 import shared
 
-struct ContentView: UIViewControllerRepresentable {
+struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
         return MainViewControllerKt.mainViewController()
     }
 
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+}
+
+struct ContentView: View {
+    var body: some View {
+        ComposeView()
+            .ignoresSafeArea(.all)
+    }
 }
