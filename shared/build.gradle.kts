1fb0320c8511001e7af0208a629bca850c39f545
Nash Nguyen <your-email@example.com>
2026-03-12 15:57:35 +0100

    fix: run desktop target on java 17

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -15,7 +15,11 @@ kotlin {
             jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
         }
     }
-    jvm("desktop")
+    jvm("desktop") {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
 
     listOf(iosX64(), iosArm64(), iosSimulatorArm64()).forEach {
         it.binaries.framework {
